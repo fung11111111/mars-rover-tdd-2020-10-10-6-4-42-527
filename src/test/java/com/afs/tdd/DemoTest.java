19201ff47872db868fc59a99c9ec89019185200d
Ray Lam <ray.lam@oocl.com>
2020-11-26 16:59:39 +0800

    immplement should_return_y_plus_1_when_executeCommand_given_00N_M()

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,22 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class DemoTest {
+
     @Test
-    void write_your_first_test() {
+    void should_return_y_plus_1_when_executeCommand_given_00N_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        //when
+        marsRover.executeCommands("M");
+
+        //then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(0,marsRover.getLocationY());
+        assertEquals(0,marsRover.getDirection());
 
     }
 }
