a03c498e403eac3e6b7eafb6ee0b512dc91d4485
Ray Lam <ray.lam@oocl.com>
2020-11-26 17:10:15 +0800

    implement should_return_x_plus_1_when_executeCommand_given_00E_M

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -17,9 +17,6 @@ public class MarsRover {
         }
     }
 
-    public void splitCommands(String commands){
-
-    }
     public int getLocationX() {
         return locationX;
     }
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -20,4 +20,17 @@ class DemoTest {
         assertEquals("N",marsRover.getDirection());
 
     }
+    @Test
+    void should_return_x_plus_1_when_executeCommand_given_00E_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"E");
+        //when
+        marsRover.executeCommands("M");
+
+        //then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(1,marsRover.getLocationY());
+        assertEquals("N",marsRover.getDirection());
+
+    }
 }
